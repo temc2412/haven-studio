f6f7f342975099bad438a9d425fb86587284aae3
ThomasMujica <temc.business@gmail.com>
2024-02-20 18:38:25 -0300

    Modificando algunos errores

--- src/components/input-layout.tsx
+++ src/components/input-layout.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const InputWithLabel = ({ label, type, ...inputProps }) => {
+const InputWithLabel = ({
+  label,
+  type,
+  ...inputProps
+}: {
+  label: string;
+  type: string;
+  [key: string]: any;
+}) => {
   return (
     <div className="flex flex-col text-gray-400 my-2">
       <label className="my-1 font-medium" htmlFor="">
         {label}
       </label>
-      <input className="my-1" type={type} {...inputProps} />
+      <input type={type} {...inputProps} />
     </div>
   );
 };
